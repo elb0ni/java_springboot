978640ad6954f2dc1575594ff40e2439471070fb
elb0ni <alenat313@gmail.com>
2024-07-01 21:50:09 -0500

    Listo mapstruct

---
+++ src/main/java/com/Boni/demo_spring/persistence/ProductoRepository.java
@@ -0,0 +1,51 @@
+package com.Boni.demo_spring.persistence;
+
+import com.Boni.demo_spring.domain.Product;
+import com.Boni.demo_spring.domain.repository.ProductRepository;
+import com.Boni.demo_spring.persistence.crud.ProductoCrudRepository;
+import com.Boni.demo_spring.persistence.entity.Producto;
+import com.Boni.demo_spring.persistence.mapper.ProductMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ProductoRepository implements ProductRepository {
+    private ProductoCrudRepository productoCrudRepository;
+    private ProductMapper mapper;
+
+    @Override
+    public List<Product> getAll(){
+        List<Producto> productos = (List<Producto>) productoCrudRepository.findAll();
+        return mapper.toProducts(productos);
+    }
+
+    @Override
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        List<Producto> productos = productoCrudRepository.findByIdCategoriaOrderByNombreAsc(categoryId);
+        return Optional.of(mapper.toProducts(productos));
+    }
+
+    @Override
+    public Optional<List<Product>> getScarseProducts(int quantity) {
+        Optional<List<Producto>> productos =  productoCrudRepository.findByCantidadStockLessThanAndEstado(quantity, true);
+        return productos.map(prods -> mapper.toProducts(prods));
+    }
+
+    @Override
+    public Optional<Product> getProduct(int productId) {
+        return productoCrudRepository.findById(productId).map(prod -> mapper.toProduct(prod));
+    }
+
+    @Override
+    public Product save(Product product) {
+        Producto producto = mapper.toProducto(product);
+        return mapper.toProduct(productoCrudRepository.save(producto));
+    }
+
+
+    public void delete(int idProduct){
+        productoCrudRepository.deleteById(idProduct);
+    }
+}
